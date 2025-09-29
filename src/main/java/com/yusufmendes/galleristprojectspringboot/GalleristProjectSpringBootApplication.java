33b76580458de420a5e88813fce4d889b2162b37
yusufmendes <yusufmendes123@gmail.com>
2025-09-29 11:36:58 +0300

    Add project files

---
+++ src/main/java/com/yusufmendes/galleristprojectspringboot/GalleristProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.yusufmendes.galleristprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleristProjectSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleristProjectSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yusufmendes/galleristprojectspringboot/GalleristProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufmendes.galleristprojectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleristProjectSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
